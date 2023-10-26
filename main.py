7bb026895b78a7212315fdcc48367560930d0429
Richard Webster <Rfwebster@gmail.com>
2023-10-26 13:17:42 +1100

    rebased to tkinter

--- main.py
+++ main.py
@@ -1,97 +1,113 @@
-# -*- coding: utf-8 -*-
-"""
-Created on 2021-11-17
-
-@author: Richard F Webster
-"""
-# Define: Start coord, end coord,
-
-# Process:
-# Ask user for start and end co-ordinates
-# take image
-# start loop while within co-ordinates
-#   move stage a bit
-#   wait for drift to settle
-#   while waiting do auto focus routine (also stigmatism, coma?)
-#   focus and stig based on max range or sigma of image - like for tomography - don't save images just read from buffer
-#   take image and save
-# end loop
-
-# UI - live image, input start and end co-ordinates, how much to move by, magnification, current stage position on chart
-# Settings - select image pix and dwell, focus pix and dwell, estimate time remaining
-
-# Anticipated problems
-#  - stitching images together, especially when there is no
-#  - need to make into a class!!!!!
-from utils import *
-import numpy as np
-
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSizePolicy, QWidget
-from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
-from PyQt5.QtTest import QTest
-from StageTracker import Ui_MainWindow
-
-import matplotlib
+import tkinter as tk
 import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, Normalize
+from matplotlib.patches import Circle
+plt.style.use('dark_background')
 
 import numpy as np
 
 try:
+    _online = True
     from PyJEM import TEM3
 except ImportError:
+    _online = False
     from PyJEM.offline import TEM3
 
-    _status = 1  # offline
-stage = TEM3.Stage3()
-coord = Coord()
+class Coord:
+    def __init__(self):
+        self.CurrentX = 0
+        self.CurrentY = 0
+        self.snake = []
+        self.snake.append([self.CurrentX, self.CurrentY])
 
-print("end of imports")
-matplotlib.use("Qt5Agg")
+    def append_snake(self):
+        self.snake.append([self.CurrentX/1000, self.CurrentY/1000])
 
 
-class MplCanvas(FigureCanvas):
-    """A canvas that updates itself every second with a new plot."""
 
-    def __init__(self, parent=None, width=10, height=10, dpi=100, data: Coord = None):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        self.data = data
-        self.compute_initial_figure()
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.coord = Coord()
 
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
+        self.setup_ui()
 
-        FigureCanvas.setSizePolicy(self,
-                                   QSizePolicy.Expanding,
-                                   QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+        self.fig = Figure(figsize=(5, 5), dpi=100)
+        self.ax = self.fig.add_subplot(111)
 
-    def compute_initial_figure(self):
-        self.axes.scatter(self.data.CurrentX, self.data.CurrentY,
-                          label="Current", c="blue", marker="x")
-        self.axes.scatter(0, 0,
-                          label="Centre", c="green", marker="P")
-        self.axes.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand",
-                         borderaxespad=0., fontsize="small")
-        self.axes.axis("equal")
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        
+        self.lim = 200
+
+        self.ax.set_xlim(-self.lim, self.lim)
+        self.ax.set_ylim(-self.lim, self.lim)
+        self.ax.set_aspect('equal')
+
+        self.ax.set_title('Snake')
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.master)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        
+        self.current_plot = self.ax.scatter(self.coord.CurrentX/1000, self.coord.CurrentY/1000,
+                                             label="Current", c="yellow", marker="x")
+        self.ax.legend(loc='upper left', fontsize="small")
+        
+        self.snake_lines = []
+        self.snake_colors = []
+        self.snake_age = []
+        self.cmap = ListedColormap(['blue', 'green', 'yellow', 'orange', 'red'])
+        self.norm = Normalize(vmin=0, vmax=50)
+
+        self.circle1 = Circle((0, 0), radius=self.lim/4, color='lawngreen', alpha=0.5, fill=False)
+        self.circle2 = Circle((0, 0), radius=self.lim/2, color='lawngreen', alpha=0.5, fill=False)
+        self.circle3 = Circle((0, 0), radius=3*self.lim/4, color='lawngreen', alpha=0.5, fill=False)
+        self.circle4 = Circle((0, 0), radius=self.lim, color='lawngreen', alpha=0.5, fill=False)
+
+        self.ax.add_patch(self.circle1)
+        self.ax.add_patch(self.circle2)
+        self.ax.add_patch(self.circle3)
+        self.ax.add_patch(self.circle4)
+        
+        self.update_plot()
+    
+    def setup_ui(self):
+        self.frame = tk.Frame(self.master)
+        self.frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.move_Button = tk.Button(self.frame, text="Move Random", command=self._move_random)
+        self.move_Button.pack(side=tk.LEFT)
+        
+        self.SpinBoxX = tk.Spinbox(self.frame, from_=0, to=1000, increment=1, width=5)
+        self.SpinBoxX.pack(side=tk.RIGHT)
+        self.SpinBoxX.bind('<Return>', self._setlim)  # bind Return key to _setlim method
+
+        self.limit_button = tk.Button(self.frame, text="Set Limit:", command=self._setlim)
+        self.limit_button.pack(side=tk.RIGHT)
+
+
+        self.NumX = tk.Label(self.frame, text="0.0")
+        self.NumX.pack(side=tk.LEFT)
+        self.NumY = tk.Label(self.frame, text="0.0")
+        self.NumY.pack(side=tk.LEFT)
+        self.update_Button = tk.Button(self.frame, text="Update", command=self._update)
+        self.update_Button.pack(side=tk.LEFT)
+
 
     def update_plot(self):
-        # Build a list of 4 random integers between 0 and 10 (both inclusive)
-        self.axes.cla()
-        self.axes.scatter(self.data.CurrentX/1000, self.data.CurrentY/1000,
-                          label="Current", c="blue", marker="x")
-        self.axes.scatter(0, 0,
-                          label="Centre", c="green", marker="P")
-        self.axes.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand",
-                         borderaxespad=0., fontsize="small")
-        self.axes.axis("equal")
-        arr = np.array(self.data.snake)
-        # print(arr.T)
+        """
+        Function to update the plot with the current position of the stage
+        and the snake
+        """
+        # self.ax.cla()
+        self.current_plot.set_offsets(np.array([self.coord.CurrentX/1000, self.coord.CurrentY/1000]))
+
+        arr = np.array(self.coord.snake)
+        print(arr.T)
 
         try:
             arr = arr.T
@@ -101,126 +117,56 @@ class MplCanvas(FigureCanvas):
             points = np.array([x, y]).T.reshape(-1, 1, 2)
             segments = np.concatenate([points[:-1], points[1:]], axis=1)
             norm = plt.Normalize(z.min(), z.max())
-            lc = LineCollection(segments, cmap='inferno', norm=norm)
+            lc = LineCollection(segments, cmap='autumn', norm=norm)
             # Set the values used for colormapping
             lc.set_array(z)
-            lc.set_linewidth(2)
-            line = self.axes.add_collection(lc)
-            #fig.colorbar(line, ax=axs[0])
-
-
-            #self.axes.plot(x, y, color="k", lw=0.25)
-        except:
+            lc.set_linewidth(0.5)
+            line = self.ax.add_collection(lc)
+        except IndexError:
             pass
-
-        self.axes.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=3, mode="expand",
-                         borderaxespad=0., fontsize="small")
-        self.axes.axis("equal")
-        self.draw()
-        
-class UpdaterThread(QObject):
-    signalExample = pyqtSignal(int, int)
-    
-    def __init__(self):
-        super().__init__()
-    
-    @pyqtSlot()
-    def update(self):
-        while True:
-            pos = stage.GetPos()
-            print("Thread: ", pos[0], pos[1])
-            coord.CurrentX, coord.CurrentY = pos[0], pos[1]
-            print("Append Snake")
-            coord.append_snake()
-            print("emit")
-            self.signalExample.emit(int(coord.CurrentX), int(coord.CurrentY))
-            print("wait)")
-            QTest.qWait(1000)
-
-
-
-class Window(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setupUi(self)
-
-        # image information
-        # TODO: get information from actual image - here just made up for debugging script
-
-
-
-        # generate microscope classes
-
-
-        # set up tracker figure
-        self.canvas_widget = QWidget(self)
-        self.canvas = MplCanvas(self.canvas_widget, width=5, height=4, dpi=100, data=coord)
-        self.tracker_holder.addWidget(self.canvas)
-
-        # set up connections
-        self.pushButton_MoveRand.clicked.connect(self._move_random)
-        self.pushButton_Go.clicked.connect(self._goto)
-
-        self.statusBar().showMessage("Idle")
-        self.updater = UpdaterThread()
-        self.updaterThread = QThread()
-        self.updaterThread.started.connect(self.updater.update)
-        self.updater.signalExample.connect(self.signalExample)
-        self.updater.moveToThread(self.updaterThread)
-        self.updaterThread.start()
-
-        #self._update()
-
-    #############################################################################
-    #
-    # Run the routine functions
-    #
-    #############################################################################
-
-    def signalExample(self, pos1, pos2):
-        print("in ui class", pos1, pos2)
-        self._update()
-
-    def _move_random(self):
-        x = np.random.randint(-100000,100000)
-        y = np.random.randint(-100000,100000)
-        #stage.SetPosition(x,y)
-        stage.SetX(x)
-        stage.SetY(y)
-        self._get_current_position()
-        self.statusBar().showMessage("X = {}; Y={}".format(int(x/1000), int(y/1000)))
-
-    def _goto(self):
-        x = float(self.SpinBoxX.value() * 1000)
-        y = float(self.SpinBoxY.value() * 1000)
-        stage.SetX(x)
-        stage.SetY(y)
-        self._get_current_position()
-        self.statusBar().showMessage("X = {}; Y={}".format(int(x/1000), int(y/1000)))
+        self.canvas.draw()
 
     ######################################################################################
     #
     # UI Buttons amd functions etc
     #
     ######################################################################################
+
+    def _move_random(self):
+        x = np.random.randint(-100000,100000)
+        y = np.random.randint(-100000,100000)
+        stage.SetX(x)
+        stage.SetY(y)
+        self._get_current_position()
+
+    def _setlim(self, lim = None):
+        x = float(self.SpinBoxX.get())
+        self.lim = int(x)
+        self.ax.set_xlim(-self.lim, self.lim)
+        self.ax.set_ylim(-self.lim, self.lim)
+        self.circle1.set_radius(self.lim/4)
+        self.circle2.set_radius(self.lim/2)
+        self.circle3.set_radius(3*self.lim/4)
+        self.circle4.set_radius(self.lim)
+        self.update_plot()
+
     def _update(self):
         #self._get_current_position()
-        self.lcdNumX.display(coord.CurrentX/1000)
-        self.lcdNumY.display(coord.CurrentY/1000)
-        self.canvas.update_plot()
+        self.NumX.configure(text=self.coord.CurrentX/1000)
+        self.NumY.configure(text=self.coord.CurrentY/1000)
+        self.update_plot()
         #QTest.qWait(1000)
 
     def _get_current_position(self):
         print("getting current position")
         pos = stage.GetPos()
         print(pos[0], pos[1])
-        coord.CurrentX, coord.CurrentY = pos[0], pos[1]
-        coord.append_snake()
+        self.coord.CurrentX, self.coord.CurrentY = pos[0], pos[1]
+        self.coord.append_snake()
         self._update()
 
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = Window()
-    win.show()
-    sys.exit(app.exec())
+    stage = TEM3.Stage3()
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
